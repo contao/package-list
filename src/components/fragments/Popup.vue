0b2797f47c7ae2d7db76b35927745190f1f21e11
Andreas Schempp <andreas.schempp@terminal42.ch>
2020-04-21 11:43:29 +0200

    Added popup component

---
+++ src/components/fragments/Popup.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="popup-overlay" @click="clearCurrent()">
+        <div ref="popup" :class="popupClass" @click.stop>
+            <slot/>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            popupClass: [String, Object],
+        },
+
+        methods: {
+            clearCurrent() {
+                this.$emit('clear');
+            },
+        },
+    };
+</script>
+
+<style lang="scss">
+    @import "../../assets/styles/defaults";
+
+    .popup-overlay {
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+
+        @include screen(960) {
+            padding: 20px 0;
+            overflow-y: auto;
+        }
+    }
+</style>
