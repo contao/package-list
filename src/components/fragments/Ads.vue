bdb7b91a2bd089c060e7d77a3aa7d813056dbc62
Andreas Schempp <andreas.schempp@terminal42.ch>
2020-06-09 18:12:55 +0200

    Show link for ads

--- src/components/fragments/Ads.vue
+++ src/components/fragments/Ads.vue
@@ -1,24 +1,27 @@
 <template>
-    <div class="container" v-if="ads && ads.length > 1">
-        <vueper-slides
-            class="no-shadow"
-            autoplay
-            infinite
-            :duration="10000"
-            :slide-ratio="1/4"
-            :visible-slides="2"
-            :breakpoints="{ 630: { slideRatio: 1/2, visibleSlides: 1 } }"
-            :bullets="false"
-            :touchable="false"
-            :arrows-outside="false"
-        >
-            <vueper-slide v-for="ad in ads" :key="ad.objectID">
-                <template #content>
-                    <a href="#" :title="ad.title" @click.stop="setCurrent(ad.package)" v-if="ad.package"><img :src="ad.image" alt=""></a>
-                    <a :href="ad.url" :title="ad.title" target="_blank" rel="noreferrer noopener" v-else><img :src="ad.image" alt=""></a>
-                </template>
-            </vueper-slide>
-        </vueper-slides>
+    <div class="ads">
+        <div class="container" v-if="ads && ads.length > 1">
+            <vueper-slides
+                class="no-shadow"
+                autoplay
+                infinite
+                :duration="10000"
+                :slide-ratio="1/4"
+                :visible-slides="2"
+                :breakpoints="{ 630: { slideRatio: 1/2, visibleSlides: 1 } }"
+                :bullets="false"
+                :touchable="false"
+                :arrows-outside="false"
+            >
+                <vueper-slide v-for="ad in ads" :key="ad.objectID">
+                    <template #content>
+                        <a href="#" :title="ad.title" @click.stop="setCurrent(ad.package)" v-if="ad.package"><img :src="ad.image" alt=""></a>
+                        <a :href="ad.url" :title="ad.title" target="_blank" rel="noreferrer noopener" v-else><img :src="ad.image" alt=""></a>
+                    </template>
+                </vueper-slide>
+            </vueper-slides>
+        </div>
+        <div class="link" v-if="$i18n.locale === 'de'"><a href="https://contao.org/de/anzeigen-erweiterungsliste.html" target="_blank">Anzeigen in der Erweiterungsliste</a></div>
     </div>
 </template>
 
@@ -46,9 +49,25 @@
         width: 100%;
     }
 
+    .ads {
+        margin: 59px 0 69px;
+    }
+
+    .link {
+        padding-top: 5px;
+        text-align: right;
+
+        a {
+            padding-left: 16px;
+            font-size: .8em;
+            color: inherit;
+            background: url("../../assets/images/link-blank.svg") left center no-repeat;
+            background-size: 13px 13px;
+        }
+    }
+
     .container {
         position: relative;
-        margin: 59px 0 69px;
         background: #FFF;
         box-shadow: 0 1px 5px 1px rgba(0, 0, 0, 0.2);
     }
