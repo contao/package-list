2d5584ff774012184b9a3f94083e69e3c9a51544
Andreas Schempp <andreas.schempp@terminal42.ch>
2020-06-23 11:52:54 +0200

    Replace the placeholder image with an SVG

--- src/components/fragments/Logo.vue
+++ src/components/fragments/Logo.vue
@@ -1,8 +1,9 @@
 <template>
     <figure :class="computedClass">
-        <!--suppress HtmlUnknownTarget -->
         <img :src="src" loading="lazy" alt="" v-if="src">
-        <img src="../../assets/images/placeholder.png" alt="" v-else>
+        <template v-else>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M4 4h7V2H4c-1.1 0-2 .9-2 2v7h2V4zm6 9l-4 5h12l-3-4-2.03 2.71L10 13zm7-4.5c0-.83-.67-1.5-1.5-1.5S14 7.67 14 8.5s.67 1.5 1.5 1.5S17 9.33 17 8.5zM20 2h-7v2h7v7h2V4c0-1.1-.9-2-2-2zm0 18h-7v2h7c1.1 0 2-.9 2-2v-7h-2v7zM4 13H2v7c0 1.1.9 2 2 2h7v-2H4v-7z"/></svg>
+        </template>
     </figure>
 </template>
 
@@ -15,9 +16,26 @@
 
         computed: {
             computedClass: vm => ({
+                'package-logo--fallback': !vm.src,
                 [vm.componentClass]: true,
                 [`${vm.componentClass}--fallback`]: !vm.src,
             }),
         },
     };
 </script>
+
+<style lang="scss" scoped>
+    .package-logo {
+        &--fallback {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background: #e9eef1;
+        }
+    }
+    svg {
+        width: 80%;
+        height: 80%;
+        fill: #babec1;
+    }
+</style>
