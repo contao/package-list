18771d153ced561a3c5316af8d18077f1a5a4fcb
Andreas Schempp <andreas.schempp@terminal42.ch>
2023-10-13 14:40:25 +0200

    Added option to disable the search input

--- src/components/fragments/SearchInput.vue
+++ src/components/fragments/SearchInput.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="search-bar">
-        <input class="search-bar__input" ref="search" id="search" type="text" :placeholder="placeholder" autocomplete="off" :value="query" @input="searchInput" @keypress.esc.prevent="stopSearch">
+        <input class="search-bar__input" ref="search" id="search" type="text" :placeholder="placeholder" :disabled="disabled" autocomplete="off" :value="query" @input="searchInput" @keypress.esc.prevent="stopSearch">
         <button class="search-bar__button search-bar__button--stop" @click="stopSearch" v-if="query">
             <svg height="24" viewBox="0 0 24 24" width="24" fill="#737373" xmlns="http://www.w3.org/2000/svg"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
         </button>
@@ -18,6 +18,7 @@
 
         props: {
             placeholder: String,
+            disabled: Boolean,
         },
 
         methods: {
