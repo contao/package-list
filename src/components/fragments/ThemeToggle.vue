39f8ff2a873e1abdefd067c7284d1e590ec9dec9
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-03-24 17:55:52 +0100

    Update theme toggle

--- src/components/fragments/ThemeToggle.vue
+++ src/components/fragments/ThemeToggle.vue
@@ -1,17 +1,11 @@
 <template>
     <div class="theme-switch">
-        <button @click="toggle" ref="themeToggle">{{ themeOptions[getColorMode()] }}</button>
-        <ul class="link-more__menu" ref="menu" v-show="visible" tabindex="-1" @blur="close" @click="close">
-            <li v-for="(label, code) in themeOptions" :key="code">
-                <a :class="[ 'theme_icon is--'+ code, { active: code === getColorMode() }]" @click="setColorMode(code)" @touchstart.stop="">{{ label }}</a>
-            </li>
-        </ul>
+        <button @click="toggleColorMode" ref="themeToggle">{{ themeOptions[getColorMode()] }}</button>
     </div>
 </template>
 
 <script>
     export default {
-
         data: () => ({
             visible: false,
         }),
@@ -26,27 +20,13 @@
         },
 
         methods: {
-            open() {
-                this.visible = true;
-                this.$nextTick(() => this.$refs.menu.focus());
-            },
-
-            close() {
-                this.$refs.menu.blur();
-                setTimeout(() => {
-                    this.visible = false;
-                }, 300);
-            },
-
-            toggle() {
-                this.visible ? this.close() : this.open();
+            toggleColorMode() {
+                this.setColorMode(this.getColorMode() === 'dark' ? 'light' : 'dark')
             },
 
             setColorMode(colorMode) {
                 document.documentElement.setAttribute('data-theme', colorMode);
                 localStorage.setItem("theme", colorMode);
-
-                //this.load(locale);
 
                 this.$refs.themeToggle.innerText = this.themeOptions[colorMode]
             },
@@ -110,81 +90,6 @@
                 content: "";
             }
         }
-
-        ul {
-            position: absolute;
-            display: block;
-            width: 200px;
-            left: 50%;
-            bottom: 25px;
-            margin: 0;
-            padding: 3px 3px 0;
-            text-align: left;
-            list-style-type: none;
-            white-space: nowrap;
-            background: var(--form-bg);
-            border: 1px solid var(--tiles-bdr);
-            border-bottom: 2px solid var(--contao);
-            transform: translateX(-50%);
-            z-index: 100;
-
-            &:after {
-                position: absolute;
-                left: 50%;
-                bottom: -6px;
-                width: 0;
-                height: 0;
-                margin-left: -4px;
-                border-style: solid;
-                border-width: 4px 3.5px 0 3.5px;
-                border-color: var(--contao) transparent transparent transparent;
-                content: "";
-            }
-        }
-
-        li {
-            margin: 0 0 3px;
-            padding: 0;
-
-            a {
-                display: flex;
-                align-items: center;
-                margin: 0;
-                padding: 5px;
-                color: var(--text);
-                cursor: pointer;
-
-                &.active {
-                    font-weight: $font-weight-bold;
-                }
-
-                &.active,
-                &:hover {
-                    color: var(--text);
-                    background: var(--focus);
-                    text-decoration: none;
-                }
-            }
-        }
-    }
-
-    .theme_icon {
-        position: relative;
-        padding-left: 25px;
-        gap: 5px;
-
-        &:before {
-            display: inline-block;
-            width: 14px;
-            height: 14px;
-            background: var(--theme-icon);
-            box-shadow: 0 0 1px var(--text);
-            border-radius: 50%;
-            content: "";
-        }
-
-        &.is--light { --theme-icon: var(--toggle-light); }
-        &.is--dark { --theme-icon: var(--toggle-dark); }
     }
 
 </style>
